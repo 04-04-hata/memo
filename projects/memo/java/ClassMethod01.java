18bda9c961458bdeab91e5c4bcfdeefb5a8b30d6
04-04-hata <zhenlinaitian@gmail.com>
2021-12-22 13:30:14 +0900

    ClassMethod01.javaにパッケージとインポートについて追記

--- projects/memo/java/ClassMethod01.java
+++ projects/memo/java/ClassMethod01.java
@@ -11,6 +11,60 @@ public class ClassMethod01 {
     }
 }
 
+〜パッケージについて〜
+
+パッケージ…クラスをまとめて分類する為の仕組み。
+
+基本構文↓
+package パッケージ名;
+複数の階層から成るパッケージの場合は、階層の上から順に.でパッケージ名を連結する。
+
+使用例↓(classMethod.humanというパッケージにHuman02というクラスがある)
+
+package classMethod.human;
+
+public class Human02 {
+	public String name;
+	public int age;
+	
+	public Human02() {
+		name = "山田";
+		age = 20;
+	}
+	
+	public Human02(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
+
+インポート…パッケージ外のクラスにアクセスする時に使う。
+対象のクラスの場所を明示する為のもの。
+
+基本構文↓
+import パッケージ名.クラス名;
+
+import パッケージ名.*
+(クラス名の所に*を使うと指定したパッケージ下の全てのクラスがインポートされる)
+
+使用例↓
+
+package classMethod;
+
+import classMethod.human.Human02;
+//class.Methodというパッケージの下humanというパッケージがあってその下にHuman02というクラスがある
+
+public class ClassMethod02 {
+	public static void main(String[] args) {
+		Human02 yamada = new Human02();
+		System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
+		
+		Human02 sato = new Human02("佐藤", 25);
+		System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
+	}
+
+}
+
 */
 
 public class ClassMethod01 {
