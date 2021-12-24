4bbb3e04d8e8a68fb49f506ac9c5be18e25e8ced
04-04-hata <zhenlinaitian@gmail.com>
2021-12-24 16:03:52 +0900

    ClassMethod01.javaにメソッドについて追記

--- projects/memo/java/ClassMethod01.java
+++ projects/memo/java/ClassMethod01.java
@@ -65,6 +65,67 @@ public class ClassMethod02 {
 
 }
 
+〜メソッドについて〜
+
+メソッド…定義した処理をさせるもの。
+
+メソッドの基本構文↓
+
+修飾子 戻り値の型　メソッド名(引数) {
+	//処理
+	return 戻り値;
+}
+
+メソッドが戻り値を返さない場合は、戻り値の型にvoidを指定。
+
+使用例↓
+
+Human03.java
+package classMethod.human;
+
+public class Human03 {
+	public String name;
+	public int age;
+	public String profession;
+	
+	public Human03() {
+		this.name = "山田";
+		this.age = 20;
+		this.profession = "プログラマー";
+		
+	}
+	//String型の戻り値を返す引数なしのメソッドを定義
+	public String getProfile() {
+		return "年齢は" + this.age + "、職業は" + this.profession;
+	}
+	//戻り値を返さない、String型の引数を一つ取るメソッドを定義
+	public void greet(String friend) {
+		if (friend == null) { //String型の値はnullの可能性があるのでチェック
+			System.out.println("挨拶する友達がわかりません!");
+			return;
+		}
+		System.out.println(friend + "さん、こんにちは!");
+	}
+}
+
+ClassMethod03.java
+package classMethod;
+
+import classMethod.human.Human03;
+
+public class ClassMethod03 {
+	public static void main(String[] args) {
+		Human03 yamada = new Human03();
+		
+		System.out.println("私の名前は、" + yamada.name + "です。");
+		String profile = yamada.getProfile();
+		System.out.println(profile + "です。");
+		
+		yamada.greet("田辺");
+		yamada.greet(null);
+	}
+}
+
 */
 
 public class ClassMethod01 {
